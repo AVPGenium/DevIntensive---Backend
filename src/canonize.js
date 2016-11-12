961299fe1488ac3dc8b835776ca599a603e767dc
Андрей Поляков <AVPGenium@yandex.ru>
2016-11-12 10:14:26 +0500

    add 2c task

--- src/canonize.js
+++ src/canonize.js
@@ -1,5 +1,5 @@
 export default function canonize(url){
-    const re = new RegExp('@?(https?:)?(\/\/)?((telegram|vk|vkontakte)[^\/]+)([a-zA-Z0-9]*)', 'i');
-    const username = url.match(re)[5];
-    return '@' + username;
+	const re = new RegExp('^(https?:)?(\/\/)?((www\.)?([a-z0-9_-]*\.)[^\/]*\/)?@?([a-z0-9\._]*)', 'i');
+	const username = url.match(re)[6].toLowerCase();
+	return '@' + username;
 }
